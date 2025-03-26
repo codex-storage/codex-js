f77978cd63837e6441993020fb90a99bb93a812d
Arnaud <arnaud@status.im>
2025-03-26 10:18:17 +0100

    Add a download example in the browser

---
+++ examples/download/esbuild.js
@@ -0,0 +1,19 @@
+const { build } = require('esbuild')
+const define = {}
+
+for (const k in process.env) {
+  define[`process.env.${k}`] = JSON.stringify(process.env[k])
+}
+
+if(!process.env["CODEX_NODE_URL"]) {
+    define[`process.env.CODEX_NODE_URL`] = "\"http://localhost:8080\""
+}
+
+const options = {
+  entryPoints: ['./index.js'],
+  outfile: './index.bundle.js',
+  bundle: true,
+  define,
+}
+
+build(options).catch(() => process.exit(1))
---
+++ examples/download/index.js
@@ -0,0 +1,14 @@
+import { Codex } from "@codex-storage/sdk-js";
+
+async function main() {
+    const codex = new Codex(process.env.CODEX_NODE_URL);
+
+    const data = codex.data
+
+    const cid = process.env.CODEX_CID
+    const result = await data.networkDownloadStream(cid);
+
+    console.info(await result.data.text())
+}
+
+main()
